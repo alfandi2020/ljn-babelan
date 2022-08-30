910919d3c40f8756347988ef37c7990b7335fb42
alfandi2020 <pandibl4530@gmail.com>
2022-08-30 08:39:52 +0700

    aaw

--- application/models/M_Auth.php
+++ application/models/M_Auth.php
@@ -4,9 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class M_Auth extends CI_Model {
     public function login($username){
         $this->db->select('*');
-        $this->db->from('users as a');
-        $this->db->join('mt_role as b','a.id=b.id_user');
-        $this->db->where('a.username',$username);
+        $this->db->from('users');
+        $this->db->where('username',$username);
         return $this->db->get()->row_array();
     }
 }
