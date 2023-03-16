b6a606cfdfc54b4c9885412de5a3e3047c1f7dc2
alfandi2020 <pandibl4530@gmail.com>
2023-03-16 14:51:52 +0700

    aa

--- application/libraries/Api_whatsapp.php
+++ application/libraries/Api_whatsapp.php
@@ -7,22 +7,25 @@ class Api_whatsapp{
     function wa_notif($msgg,$phonee)
     {
     $sender = 'mahfud';
+    $phone = $phonee;
+    $msg = $msgg;
         // if ($sender == "mahfud") {
                 // $token = "rasJFCC37ewayax21uu2Caog9CCqyT3KSwBWFqQAbQMdMAefxa";
                 // $phone = $phone; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
-                // $message = $msg;
+                $message = $msg;
+                $curl = curl_init();
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
-                    CURLOPT_URL => 'https://ljn.fantecno.net/cron/send',
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => '',
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'POST',
-                    CURLOPT_POSTFIELDS => 'key='.$sender.'&phone='.$phonee.'&msg='.$msgg,
-                  ));
+                  CURLOPT_URL => 'https://ljn.fantecno.net/cron/send',
+                  CURLOPT_RETURNTRANSFER => true,
+                  CURLOPT_ENCODING => '',
+                  CURLOPT_MAXREDIRS => 10,
+                  CURLOPT_TIMEOUT => 0,
+                  CURLOPT_FOLLOWLOCATION => true,
+                  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                  CURLOPT_CUSTOMREQUEST => 'POST',
+                  CURLOPT_POSTFIELDS => 'key='.$sender.'&phone='.$phone.'&msg='.$msgg,
+                ));
                 $response = curl_exec($curl);
                 curl_close($curl);
                 return $response;
