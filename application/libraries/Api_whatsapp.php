737e452a7a5c38e62f9145867eb78ea9dcbd3f4c
alfandi2020 <pandibl4530@gmail.com>
2023-03-16 14:44:28 +0700

    aa

--- application/libraries/Api_whatsapp.php
+++ application/libraries/Api_whatsapp.php
@@ -15,19 +15,19 @@ class Api_whatsapp{
                 $message = $msg;
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
-                CURLOPT_URL => 'http://103.171.85.211:8000/send-message',
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS => 'sender='.$sender.'&number='.$phone.'&message='.$message,
-                ));
+                    CURLOPT_URL => 'https://ljn.fantecno.net/cron/send',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => 'key='.$sender.'&phone='.$phone.'&msg='.$msgg,
+                  ));
                 $response = curl_exec($curl);
                 curl_close($curl);
-                return $curl;
+                return $response;
                 // redirect('permohonan/index/'.$this->hash_url->base64_url_encode($otp).'/'.$this->hash_url->base64_url_encode($phone));
         }
     }
