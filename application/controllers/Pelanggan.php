cb30465f4f7ad9e2fca0f5702335b4b04539188f
alfandi2020 <pandibl4530@gmail.com>
2023-03-21 16:37:34 +0700

    awfa

--- application/controllers/Pelanggan.php
+++ application/controllers/Pelanggan.php
@@ -351,12 +351,12 @@ Layanan Teknis	:
             // $mpdf->defaultfooterline=0;
             // $mpdf->setFooter('<div style="text-align: left;">F.7.1.1</div>');
             $mpdf->WriteHTML($html);
-            // $mpdf->Output('invoice/'.$no_invoice.'.pdf','F');
-            $mpdf->Output();
-			// $imagick = new Imagick();
-            // $imagick->setResolution(400, 400);
-            // $imagick->readImage("invoice/$no_invoice.pdf");
-            // $imagick->writeImages("invoice/image/$no_invoice.jpg", false);
+            $mpdf->Output('invoice/'.$no_invoice.'.pdf','F');
+            // $mpdf->Output();
+			$imagick = new Imagick();
+            $imagick->setResolution(400, 400);
+            $imagick->readImage("invoice/$no_invoice.pdf");
+            $imagick->writeImages("invoice/image/$no_invoice.jpg", false);
 		}
 	}
 	function info()
