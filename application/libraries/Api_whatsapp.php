cbc2489714136bf234bc03044fc5cc9cdcacf471
alfandi2020 <pandibl4530@gmail.com>
2023-03-21 17:15:57 +0700

    a

--- application/libraries/Api_whatsapp.php
+++ application/libraries/Api_whatsapp.php
@@ -6,14 +6,13 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Api_whatsapp{
     function wa_notif($msgg,$phonee)
     {
-    $sender = 'mahfud';
-    $phone = $phonee;
-    $msg = $msgg;
+        $sender = 'mahfud';
+        $phone = $phonee;
+        $msg = $msgg;
         // if ($sender == "mahfud") {
                 // $token = "rasJFCC37ewayax21uu2Caog9CCqyT3KSwBWFqQAbQMdMAefxa";
                 // $phone = $phone; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
                 $message = $msg;
-                $curl = curl_init();
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
                   CURLOPT_URL => 'https://ljn.fantecno.net/cron/send',
@@ -25,6 +24,33 @@ class Api_whatsapp{
                   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                   CURLOPT_CUSTOMREQUEST => 'POST',
                   CURLOPT_POSTFIELDS => 'key='.$sender.'&phone='.$phone.'&msg='.$msgg,
+                ));
+                $response = curl_exec($curl);
+                curl_close($curl);
+                return $response;
+                // redirect('permohonan/index/'.$this->hash_url->base64_url_encode($otp).'/'.$this->hash_url->base64_url_encode($phone));
+        // }
+    }
+    function wa_notif_doc($msgg,$phonee,$file)
+    {
+        $sender = 'mahfud';
+        $phone = $phonee;
+        $msg = $msgg;
+        // if ($sender == "mahfud") {
+                // $token = "rasJFCC37ewayax21uu2Caog9CCqyT3KSwBWFqQAbQMdMAefxa";
+                // $phone = $phone; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
+                $message = $msg;
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                  CURLOPT_URL => 'https://ljn.fantecno.net/cron/send_doc',
+                  CURLOPT_RETURNTRANSFER => true,
+                  CURLOPT_ENCODING => '',
+                  CURLOPT_MAXREDIRS => 10,
+                  CURLOPT_TIMEOUT => 0,
+                  CURLOPT_FOLLOWLOCATION => true,
+                  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                  CURLOPT_CUSTOMREQUEST => 'POST',
+                  CURLOPT_POSTFIELDS => 'key='.$sender.'&phone='.$phone.'&msg='.$msgg.'&file='.$file,
                 ));
                 $response = curl_exec($curl);
                 curl_close($curl);
