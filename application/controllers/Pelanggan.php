68d3db50d948b6dfe314980601468f1da1e0a28e
alfandi2020 <pandibl4530@gmail.com>
2023-08-05 19:06:48 +0700

    aa

--- application/controllers/Pelanggan.php
+++ application/controllers/Pelanggan.php
@@ -416,7 +416,7 @@ Kantor Layanan Babelan
 Layanan Teknis	: 
 0821-1420-9923
 0819-3380-3366";
-			if (file_exists($url_img)) {
+			if (!file_exists($url_img)) {
 				$this->api_whatsapp->wa_notif_doc($msg,$get_client['telp'],$url_img);
 			}else{
 				echo 1;
