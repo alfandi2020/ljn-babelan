7cf0be79d25cbbe0a3603d0f39825ba20cd7dfd0
alfandi2020 <pandibl4530@gmail.com>
2024-05-26 19:33:19 +0700

    a

--- application/controllers/Pelanggan.php
+++ application/controllers/Pelanggan.php
@@ -272,6 +272,7 @@ class Pelanggan extends CI_Controller {
 				$mpdf->Output('/home/billing.lintasmediadata.net/invoice/struk/' . $no_invoice . '.pdf', 'F');
 				// chmod($no_invoice . ".pdf", 0777);
 				// $mpdf->Output();
+				sleep(3);
 				$imagick = new Imagick();
 				$imagick->setResolution(200, 200);
 				$imagick->readImage("invoice/struk/$no_invoice.pdf");
