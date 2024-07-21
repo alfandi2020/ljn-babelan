d1d318a63d8aa2a89bda4ebf3243eb3874bbd593
alfandi2020 <pandibl4530@gmail.com>
2024-07-21 16:32:42 +0700

    a

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -158,7 +158,7 @@ Layanan Teknis	:
                 // if (is_array($neko) || is_object($neko)){
             // var_dump(json_decode($neko));
             $datax = json_encode($neko,true);
-            $x = json_decode($datax);
+            $x = ($datax);
             var_dump($x);
                     foreach(json_decode($datax) as $jquin) {
                 echo substr($jquin['amount'], -3);
