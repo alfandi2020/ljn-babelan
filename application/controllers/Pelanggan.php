ebcafa890fc8906dabd18c35252e71c7b3671268
alfandi2020 <pandibl4530@gmail.com>
2024-08-05 21:34:32 +0700

    a

--- application/controllers/Pelanggan.php
+++ application/controllers/Pelanggan.php
@@ -1061,10 +1061,10 @@ Layanan Teknis	:
 		$kd_unik_in = sprintf('%04d', $kd_unik_in);
 		$pay = $this->db->get_where('mt_payment',['id_pelanggan' => $id ]);
 		// $payment =+ 'BCA 2761446578 an Mahfudin';
-		// $payment = '';
+		$payment = '';
 		if ($pay->num_rows() > 1) {
             foreach ($pay->result() as $k) {
-                $payment = $k->company . ' ' . $k->va . ' an' . $get_client['nama'] . "<br>" ;
+                $payment =+ $k->company . ' ' . $k->va . ' an' . $get_client['nama'] . "<br>" ;
             }
         }else{
             $payment = 'MANDIRI 1560016047112 an Mahfudin';
